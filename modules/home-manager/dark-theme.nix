13e901460f54d25a3b3de7b71f843b1b8ed96412
installer <installer@gmail.com>
2026-08-14 22:19:57 +0200

    Added new files to split responsibilites over packages and dark-theme in the system

---
+++ modules/home-manager/dark-theme.nix
@@ -0,0 +1,20 @@
+{ self, inputs, ... }: {
+
+  flake.modules.homeManager.dark-theme = { pkgs, lib, ... }: {
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
+    };
+
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";   # to jest kluczowe dla apek libadwaita (GTK4)
+        gtk-theme = "Adwaita-dark";     # to dla starszych apek GTK3
+      };
+    };
+
+  };
+}
