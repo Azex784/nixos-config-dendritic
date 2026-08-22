91ff15abfa9bef8eb912714b7d6daebdb96254cd
installer <installer@gmail.com>
2026-08-22 10:52:32 +0200

    Improved overall project file structure. Added gaming workspace and changed keybinds in niri.

--- hosts/Sarasti/hardware-configuration.nix
+++ hosts/Sarasti/hardware-configuration.nix
@@ -10,19 +10,6 @@
     boot.kernelModules = [ "kvm-intel" ];
     boot.extraModulePackages = [ ];
 
-    fileSystems."/" = {
-      device = "/dev/mapper/cryptnixos";
-      fsType = "btrfs";
-      };
-
-    #boot.initrd.luks.devices."cryptnixos".device = "/dev/disk/by-uuid/7cf4b060-844a-44e1-be3f-52365b365b24";
-
-    fileSystems."/nix" ={
-        device = "/dev/mapper/cryptnixos";
-        fsType = "btrfs";
-        options = [ "subvol=nix" ];
-    };
-
     boot.initrd.luks.devices."cryptnixos" = {
         device = "/dev/disk/by-uuid/7cf4b060-844a-44e1-be3f-52365b365b24";
         keyFile = "/cryptkey";
@@ -33,10 +20,29 @@
         name = "cryptkey";
     };
 
+    fileSystems."/" = {
+      device = "/dev/mapper/cryptnixos";
+      fsType = "btrfs";
+      };
+
+    # boot.initrd.luks.devices."cryptnixos".device = "/dev/disk/by-uuid/7cf4b060-844a-44e1-be3f-52365b365b24";
+
+    fileSystems."/nix" ={
+        device = "/dev/mapper/cryptnixos";
+        fsType = "btrfs";
+        options = [ "subvol=nix" ];
+    };
+
     fileSystems."/home" =
       { device = "/dev/mapper/cryptnixos";
         fsType = "btrfs";
         options = [ "subvol=home" ];
+      };
+
+    fileSystems."/opt" =
+      { device = "/dev/mapper/cryptnixos";
+        fsType = "btrfs";
+        options = [ "subvol=opt" ];
       };
 
     fileSystems."/boot" =
---
+++ modules/nixos/services.nix
@@ -0,0 +1,26 @@
+{ self, inputs, ... }: {
+
+  flake.modules.nixos.services = { pkgs, lib, ... }: {
+    # Services
+    services = {
+      # Enable the X11 windowing system.
+      xserver.enable = true;
+      # Enable the OpenSSH daemon.
+      openssh.enable = true;
+      # Configure keymap in X11
+      xserver.xkb = {
+          layout = "pl";
+          variant = "";
+          options = "";
+      };
+      # Enable CUPS to print documents.
+      printing.enable = true;
+      # Enable sound
+      pipewire = {
+        enable = true;
+        pulse.enable = true;
+      };
+    };
+
+  };
+}
